a81f9a36d180b9bd12e469be20e9bb68c1cae5e5
glebgordeevqa <gleb.gordeev@playrix.com>
2019-10-05 23:11:28 +0300

    Update test

--- test_items.py
+++ test_items.py
@@ -7,10 +7,10 @@ def test_button(browser, request):
     lang = request.config.getoption("language")
     if lang == "fr":
         check = len(browser.find_element_by_css_selector("button.btn.btn-lg.btn-primary.btn-add-to-basket"))
-        assert check>0, f"Button not found :("
+        assert check != 0, f"Button not found :("
     elif lang == "es":
         check = len(browser.find_elements_by_css_selector("button.btn.btn-lg.btn-primary.btn-add-to-basket"))
-        assert check>0, f"Button not found :("
+        assert check != 0, f"Button not found :("
     else:
         raise pytest.UsageError("--language should be fr or es "
                                 "(Тест не обязательно должен поддерживать все языки по задаче)")
