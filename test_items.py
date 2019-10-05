e7e6402c513a4e1381729952d8c00fa154bfd828
glebgordeevqa <gleb.gordeev@playrix.com>
2019-10-05 22:51:24 +0300

    Update test

--- test_items.py
+++ test_items.py
@@ -3,15 +3,14 @@ import time
 
 def test_button(browser, request):
     browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+    browser.implicitly_wait(10)
     lang = request.config.getoption("language")
     if lang == "fr":
-        browser.implicitly_wait(10)
-        check = browser.find_element_by_css_selector("button.btn-lg.btn-primary[value='Ajouter au panier']")
-        assert check != "Ajouter au panier", f"Button not found :("
+        check = browser.find_element_by_css_selector("button.btn.btn-lg.btn-primary.btn-add-to-basket").text
+        assert check == "Ajouter au panier", f"Button not found :("
     elif lang == "es":
-        browser.implicitly_wait(10)
-        check = browser.find_element_by_css_selector("button.btn-lg.btn-primary[value='Añadir al carrito']")
-        assert check != "Añadir al carrito", f"Button not found :("
+        check = browser.find_element_by_css_selector("button.btn.btn-lg.btn-primary.btn-add-to-basket").text
+        assert check == "Añadir al carrito", f"Button not found :("
     else:
         raise pytest.UsageError("--language should be fr or es "
                                 "(Тест не обязательно должен поддерживать все языки по задаче)")
